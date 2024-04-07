564e4b13cd2484d8663ba18b04ef5524961150d4
eXory2024 <exory@anio.software>
2024-04-07 05:57:04 +0200

    Export used runtime code.

--- injectRuntimeCode.mjs
+++ injectRuntimeCode.mjs
@@ -9,11 +9,11 @@ const __dirname = path.dirname(
 let dist_runtime_path = path.join(__dirname, "dist", "plugin.mjs")
 let dist_runtime = (await fs.readFile(dist_runtime_path)).toString()
 
-dist_runtime = dist_runtime.split(`"%%%VIPEN_RUNTIME_CODE%%%"`).join(
-	JSON.stringify(
-		(await fs.readFile(path.join(__dirname, "node_modules", "@vipen", "js-and-web-runtime", "dist", "runtime.mjs"))).toString()
-	)
-)
+const runtime_code = (await fs.readFile(path.join(__dirname, "node_modules", "@vipen", "js-and-web-runtime", "dist", "runtime.mjs"))).toString()
+
+dist_runtime = dist_runtime.split(`"%%%VIPEN_RUNTIME_CODE%%%"`).join(JSON.stringify(runtime_code))
 
 await fs.writeFile(`${dist_runtime_path}.tmp`, dist_runtime)
 await fs.rename(`${dist_runtime_path}.tmp`, dist_runtime_path)
+
+await fs.writeFile(path.join(__dirname, "dist", "runtime.mjs"), runtime_code)
